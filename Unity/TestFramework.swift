75bdb6b504e2b8e2048e57c3eb8f95db1010cbba
Shada <shada.lutfiabdalla@appfoundry.be>
2018-04-11 23:11:04 +0200

    add test

---
+++ Unity/TestFramework.swift
@@ -0,0 +1,18 @@
+//
+//  TestFramework.swift
+//  Unity
+//
+//  Created by Shada Lutfi on 11/04/2018.
+//  Copyright © 2018 AppFoundry. All rights reserved.
+//
+
+import Foundation
+
+
+class TestFramework {
+    
+    func test() {
+        print("test framework")
+    }
+    
+}
